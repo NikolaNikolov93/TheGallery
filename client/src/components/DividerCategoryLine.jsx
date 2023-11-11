bd4be6338db15bc61a3130f8e4feef5298c3405b
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-11-11 12:08:37 +0200

    TheGallery - Fixed divider css

--- client/src/components/DividerCategoryLine.jsx
+++ client/src/components/DividerCategoryLine.jsx
@@ -1,4 +1,8 @@
 import styles from "./DividerCategoryLine.module.css";
 export default function DividerCategoryLine() {
-    return <p className={styles.divider}>CATEGORIES </p>;
+    return (
+        <div className={styles.divider}>
+            <p className={styles.text}>CATEGORIES </p>
+        </div>
+    );
 }
