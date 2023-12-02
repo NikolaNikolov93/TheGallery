84a5ccd64665325e2b38408b1f3997a7ccebcba9
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-12-02 11:35:05 +0200

    Remove authContext from App to authContextJSX

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
-import * as authService from "./services/authService";
 import Categories from "./components/categories/Categories";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import InsideSingleCategory from "./components/inside-single-categorie/InsideSingleCategory";
 import NotFound from "./components/notFound/NotFound";
 import { useState, useEffect } from "react";
-import AuthContext from "./components/contexts/authContext";
+import { AuthProvider } from "./components/contexts/authContext";
 import CategoriesContext from "./components/contexts/categoriesContext";
-import Path from "./paths";
 import Top10 from "./components/top10/top10";
 import CreatePicture from "./components/createPicture/CreatePicture";
 import * as categoriesServices from "./services/categoriesServices";
 
 function App() {
-    const navigate = useNavigate();
     const [categories, setCategoreis] = useState([]);
     useEffect(() => {
         categoriesServices
@@ -23,73 +20,6 @@ function App() {
             .catch((err) => console.log(err));
     }, []);
 
-    /**Authentication state */
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem("accessToken");
-        return {};
-    });
-
-    /** Lifted state for the Login From */
-    const [showLogin, setShowLogin] = useState(false);
-    const closeLoginModal = () => {
-        setShowLogin(false);
-    };
-    const openLoginModal = () => {
-        setShowLogin(true);
-    };
-
-    const loginSubmitHandler = async (values) => {
-        const result = await authService.login(values.email, values.password);
-        setAuth(result);
-        localStorage.setItem("accessToken", result.accessToken);
-        navigate(Path.Home);
-        closeLoginModal();
-    };
-
-    /** Lifted state for the Register From */
-    const [showRegister, setShowRegister] = useState(false);
-    const closeRegisterModal = () => {
-        setShowRegister(false);
-    };
-    const openRegisterModal = () => {
-        setShowRegister(true);
-    };
-    const registerSubmitHandler = async (values) => {
-        const result = await authService.register(
-            values.email,
-            values.password,
-            values.username
-        );
-        setAuth(result);
-        localStorage.setItem("accessToken", result.accessToken);
-        navigate(Path.Home);
-        closeRegisterModal();
-    };
-
-    /**Logout handler */
-    const logoutHandler = async (token) => {
-        await authService.logout(token);
-        setAuth({});
-        localStorage.removeItem("accessToken");
-        navigate(Path.Home);
-    };
-
-    /**Authentication values object */
-    const authenticationContextData = {
-        loginSubmitHandler,
-        closeLoginModal,
-        openLoginModal,
-        showLogin,
-        closeRegisterModal,
-        openRegisterModal,
-        showRegister,
-        registerSubmitHandler,
-        logoutHandler,
-        username: auth.username,
-        email: auth.email,
-        isAuthenticated: !!auth.accessToken,
-        token: auth.accessToken,
-    };
     /**Categoreis values object  */
     const categoriesContextData = {
         categories,
@@ -97,8 +27,8 @@ function App() {
 
     return (
         <>
-            <CategoriesContext.Provider value={categoriesContextData}>
-                <AuthContext.Provider value={authenticationContextData}>
+            <AuthProvider>
+                <CategoriesContext.Provider value={categoriesContextData}>
                     <Header />
                     <main className="main">
                         <Routes>
@@ -116,8 +46,8 @@ function App() {
                         </Routes>
                     </main>
                     <Footer />
-                </AuthContext.Provider>
-            </CategoriesContext.Provider>
+                </CategoriesContext.Provider>
+            </AuthProvider>
         </>
     );
 }
---
+++ client/src/components/contexts/authContext.jsx
@@ -0,0 +1,86 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as authService from "../../services/authService";
+import Path from "../../paths";
+
+const AuthContext = createContext();
+AuthContext.displayName = "AuthContext";
+
+export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate();
+
+    /**Authentication state */
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem("accessToken");
+        return {};
+    });
+
+    /** Lifted state for the Login From */
+    const [showLogin, setShowLogin] = useState(false);
+    const closeLoginModal = () => {
+        setShowLogin(false);
+    };
+    const openLoginModal = () => {
+        setShowLogin(true);
+    };
+
+    const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+        setAuth(result);
+        localStorage.setItem("accessToken", result.accessToken);
+        navigate(Path.Home);
+        closeLoginModal();
+    };
+
+    /** Lifted state for the Register From */
+    const [showRegister, setShowRegister] = useState(false);
+    const closeRegisterModal = () => {
+        setShowRegister(false);
+    };
+    const openRegisterModal = () => {
+        setShowRegister(true);
+    };
+    const registerSubmitHandler = async (values) => {
+        const result = await authService.register(
+            values.email,
+            values.password,
+            values.username
+        );
+        setAuth(result);
+        localStorage.setItem("accessToken", result.accessToken);
+        navigate(Path.Home);
+        closeRegisterModal();
+    };
+
+    /**Logout handler */
+    const logoutHandler = async (token) => {
+        await authService.logout(token);
+        setAuth({});
+        localStorage.removeItem("accessToken");
+        navigate(Path.Home);
+    };
+
+    /**Authentication values object */
+    const authenticationContextData = {
+        loginSubmitHandler,
+        closeLoginModal,
+        openLoginModal,
+        showLogin,
+        closeRegisterModal,
+        openRegisterModal,
+        showRegister,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username,
+        email: auth.email,
+        isAuthenticated: !!auth.accessToken,
+        token: auth.accessToken,
+    };
+    return (
+        <AuthContext.Provider value={authenticationContextData}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
