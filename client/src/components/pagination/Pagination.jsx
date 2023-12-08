5224717d94686299bbfd680d452ab18488e6f816
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-12-09 00:28:20 +0200

    Fixed pagination

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,19 +1,36 @@
 import { Link } from "react-router-dom";
 import styles from "../../components/pagination/Pagination.module.css";
-export default function Pagination({ picsPerPage, totalPics, paginate }) {
+export default function Pagination({
+    picsPerPage,
+    totalPics,
+    paginate,
+    currentPage,
+}) {
     const pageNumbers = [];
     for (let i = 1; i <= Math.ceil(totalPics / picsPerPage); i++) {
         pageNumbers.push(i);
     }
+
     return (
         <nav>
             <ul className={styles["pagination"]}>
                 {pageNumbers.map((number) => (
                     <li key={number} className={styles["page-item"]}>
                         <Link
-                            onClick={() => paginate(number)}
-                            className={styles["page-link"]}
+                            onClick={(e) => {
+                                paginate(number, e);
+                            }}
+                            className={
+                                styles[
+                                    `${
+                                        currentPage == number
+                                            ? "active"
+                                            : "page-link"
+                                    }`
+                                ]
+                            }
                             href="!#"
+                            id={`${number}`}
                         >
                             {number}
                         </Link>
